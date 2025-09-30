5f75fba7ecc34f24f6339ee864a523a1250bed75
Edvard Dingsør <edvard.wdingsor@gmail.com>
2025-09-30 11:57:48 +0200

    feat: finish spi driver (I think)

--- include/drivers/spi.h
+++ include/drivers/spi.h
@@ -4,26 +4,27 @@
 #include "avr/io.h"
 
 
-
-#define IO_SS_PIN PB4
-#define DISP_SS_PIN PB3
-
-#define MOSI_PIN PB5
-#define DD_MOSI DDB5
-
-#define MISO_PIN PB6
-#define DD_MISO DDB6
-
-#define SCK_PIN PB7
-#define DD_SCK DDB7
-
 #define DDR_SPI DDRB
 
 #define DATA_N_C PB1
 #define RND_SS PB2
+#define DISP_SS_PIN PB3
+#define IO_SS_PIN PB4
+#define MOSI_PIN PB5
+#define MISO_PIN PB6
+#define SCK_PIN PB7
+
 
 void spi_master_init();
-void spi_master_transmit(char data);
+
+void spi_master_transmit_byte(char data);
+void spi_master_transmit_bytes(char* data, uint16_t length);
+
+char spi_master_read_byte();
+void spi_master_read_bytes(char* read_buffer, uint16_t length);
+
+void spi_master_select_slave(uint8_t slave_id);
+
 
 ///////////////////////////////////////////////////////////////////
 #endif
--- src/drivers/spi.c
+++ src/drivers/spi.c
@@ -1,19 +1,49 @@
 #include "drivers/spi.h"
 
 
-void spi_master_init() {
-    // Set MOSI and SCK output, rest input
-    DDR_SPI = (1 << DD_MOSI) | (1 << DD_SCK) | (0b1111 << 1);
+void spi_master_init(){
+    // Set MOSI, SCK, IO_SS, DISP_SS, RND_SS and DATA_N_C as output, rest input
+    DDR_SPI = (1 << MOSI_PIN) | (1 << SCK_PIN) | (1 << IO_SS_PIN) | 
+            (1 << DISP_SS_PIN) | (1 << RND_SS) | (1 << DATA_N_C);
     
     // Enable SPI, Master, set clock rate fck / 16
     SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0);
 
 }
 
-void spi_master_transmit(char data) {
-    // Start transmission
-    SPDR = data;
 
+void spi_master_transmit_byte(char data){
+    SPDR = data; // Start transmission
     while (!(SPSR & (1 << SPIF))); // Wait until it completes
 }
 
+
+void spi_master_transmit_bytes(char* data, uint16_t length){
+    // Transmit multiple bytes
+    for (uint16_t i = 0; i < length; i++){
+        spi_master_transmit_byte(data[i]);
+    }
+}
+char spi_master_read_byte() {
+    while (!(SPSR & (1 << SPIF))); // Wait for reception complete
+    return SPDR;
+}
+
+
+void spi_master_read_bytes(char* read_buffer, uint16_t length){
+    // Read multiple bytes
+    for (uint16_t i = 0; i < length; i++){
+        SPDR = 0x43; // Send dummy byte
+        read_buffer[i] = spi_master_read_byte();
+    }
+    // read_buffer is now filled
+}
+
+
+void spi_master_select_slave(uint8_t slave_id){
+    // Initially, turn of all slaves by driving them high
+    PORTB |= (1 << IO_SS_PIN) | (1 << DISP_SS_PIN) | (1 << RND_SS);
+    
+    // Turn on selected slave by driving it low
+    PORTB &= ~(1 << slave_id);
+}
