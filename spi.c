5420608405c9176fb9053eab75f54d5bb5ea1add
Einar Augestad <einaraug@stud.ntnu.no>
2025-09-30 09:34:54 +0200

    wip: create basic spi methods

--- spi.c
+++ spi.c
@@ -1,3 +1,32 @@
 #include "spi.h"
 
-void spi_init();
+
+void spi_master_init() {
+    // Set MOSI and SCK output, rest input
+    DDR_SPI = (1 << DD_MOSI) | (1 << DD_SCK);
+    
+    // Enable SPI, Master, set clock rate fck / 16
+    SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0);
+}
+
+void spi_master_transmit(char data) {
+    // Start transmission
+    SPDR = data;
+
+    while (!(SPSR & (1 << SPIF))); // Wait until it completes
+}
+
+
+
+void spi_slave_init() {
+    // Set MISO output, rest input
+    DDR_SPI = (1 << DD_MISO);
+
+    // Enable SPI
+    SPCR = (1 << SPE);
+}
+
+char spi_slave_receive() {
+    while (!(SPSR & (1 << SPIF))); // Wait unit reception complete
+    return SPDR;
+}
--- spi.h
+++ spi.h
@@ -7,13 +7,24 @@
 
 #define IO_SS_PIN PB4
 #define DISP_SS_PIN PB3
+
 #define MOSI_PIN PB5
+#define DD_MOSI DDB5
+
 #define MISO_PIN PB6
+#define DD_MISO DDB6
+
 #define SCK_PIN PB7
+#define DD_SCK DDB7
 
+#define DDR_SPI DDRB
 
+void spi_master_init();
+void spi_master_transmit();
 
-void spi_init();
+void spi_slave_init();
+char spi_slave_receive();
+
 
 ///////////////////////////////////////////////////////////////////
 #endif
