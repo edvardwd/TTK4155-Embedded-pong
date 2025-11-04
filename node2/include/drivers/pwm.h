416515fd6f6cac9a1e1e05e4d3fa501e8b4dd603
Matheus Ullmann <matheusu@stud.ntnu.no>
2025-11-04 12:54:52 +0100

    wip: pwm driver

--- node2/include/drivers/pwm.h
+++ node2/include/drivers/pwm.h
@@ -1,2 +1,9 @@
 #include "component/component_pwm.h"
 #include "instance/instance_pwm.h"
+#include"component/component_pmc.h"
+#include "instance/instance_pmc.h"
+#include "sam3x8e.h"
+
+
+
+void pwm_init();
---
+++ node2/src/drivers/pwm.c
@@ -0,0 +1,7 @@
+#include "drivers/pwm.h"
+
+void pwm_init(){
+    // Activate peripheral device clock
+    PMC_PCER1_PID36 = 
+    
+}
